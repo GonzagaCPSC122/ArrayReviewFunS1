99d8a16245200305098e03b20433ca28ef74b54f
gsprint23 <sprint@gonzaga.edu>
2020-01-16 12:32:54 -0800

    add solution to 2D array

--- Main.cpp
+++ Main.cpp
@@ -8,15 +8,21 @@ const int ROWS = 4;
 const int COLS = 5;
 
 void fillArray(int[], int);
+void fillArray(int[][COLS], int, int);
 void printArray(int[], int);
+void printArray(int[][COLS], int, int);
 
 int main() {
 	int oneD[COLS];
+	int twoD[ROWS][COLS];
 	
 	srand(time(0));
 	
 	fillArray(oneD, COLS);
 	printArray(oneD, COLS);
+	cout << endl;
+	fillArray(twoD, ROWS, COLS);
+	printArray(twoD, ROWS, COLS);
 	return 0;
 }
 
@@ -34,11 +40,36 @@ void fillArray(int arr[], int size) {
 	} while (i < size);
 }
 
+void fillArray(int arr[][COLS], int rows, int cols) {
+	int i = 0, j = 0;
+	
+	do {
+		j = 0; // don't forget this!!
+		do {
+			arr[i][j] = rand() % 100 + 1;
+			j++;
+		} while (j < cols);
+		i++;
+	} while (i < rows);
+}
+
 void printArray(int arr[], int size) {
 	int i;
 	
 	for (i = 0; i < size; i++) {
 		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void printArray(int arr[][COLS], int rows, int cols) {
+	int i, j;
+	
+	for (i = 0; i < rows; i++) {
+		for (j = 0; j < cols; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
 	}
 	cout << endl;
 }
